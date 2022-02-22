f1e1d103f1c70ce094f67de932f6bd023cf55237
AnDCGI <69905741+AnDCGI@users.noreply.github.com>
2022-02-22 17:29:22 +0530

    Create aiMatte.py
    Arnold Set Matte To "On" For All* Objects In The Scene Useful For FX Volume Renders

---
+++ py-scripts/ai_MtoA/aiMatte.py
@@ -0,0 +1,13 @@
+# © 2020 AnD CGI This work is licensed under a Creative Commons
+# Attribution-ShareAlike 4.0 International License.
+
+import maya.cmds as cmds  # Importing The Main Maya Python Module
+
+# *Selecting Mesh, Nurbs Surfaces & Subdivition Surfaces Only, Not Everything
+# In The Scene.
+sel = cmds.ls(ni=1, typ=["mesh", "nurbsSurface", "subdiv"])
+num = len(sel)
+for i in range(0, num):  # For Loop On Selection
+    # Sets On Or Off Status
+    selAttr = sel[i] + ".aiMatte"
+    cmds.setAttr(selAttr, 1)
