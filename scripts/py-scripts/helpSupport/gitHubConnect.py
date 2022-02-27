8b4dfcde6126e6549153b72b5713f97341f545cc
AnDCGI <69905741+AnDCGI@users.noreply.github.com>
2022-02-27 19:09:51 +0530

    Shelf Updates

---
+++ scripts/py-scripts/helpSupport/gitHubConnect.py
@@ -0,0 +1,8 @@
+import maya.cmds as cmds
+
+
+def showHelp():
+    cmds.showHelp("https://github.com/AnDCGI", absolute=True)
+
+
+showHelp()
---
+++ scripts/py-scripts/helpSupport/kofiConnect.py
@@ -0,0 +1,8 @@
+import maya.cmds as cmds
+
+
+def showHelp():
+    cmds.showHelp("https://ko-fi.com/andcgi", absolute=True)
+
+
+showHelp()
